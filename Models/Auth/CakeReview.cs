25e45740408a74b0c6f0819246898a68753d7f70
Amin <83289593+amin98karimov@users.noreply.github.com>
2025-05-15 16:44:30 +0500

    Update CakeReview.cs

--- Models/Auth/CakeReview.cs
+++ Models/Auth/CakeReview.cs
@@ -6,7 +6,7 @@ public class CakeReview
     public int UserId { get; set; }
     public int CakeId { get; set; }
     public string Comment { get; set; } = "";
-    public int Rating { get; set; } // 1 to 5
+    public int Rating { get; set; } 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public User User { get; set; } = null!;
